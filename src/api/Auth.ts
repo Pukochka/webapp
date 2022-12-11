7b10af7bb7e872f19d1f253708a72f56499b5782
Pukochka <tema.puko@gmail.com>
2022-12-12 00:51:37 +0300

    fix

--- src/api/Auth.ts
+++ src/api/Auth.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import { config } from 'src/config';
 
 export default async function (auth_str: string) {
   try {
     return await axios.post(
       `https://api.bot-t.com/v1/module/bot/check-hash?${auth_str}`,
       {
-        bot_id: 12845,
+        bot_id: config.BOT_ID,
       }
     );
   } catch (e) {}
--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,23 @@ declare global {
 
 window.Telegram = window.Telegram || {};
 
+let bot_id = '';
+let secret = '';
+let init = '';
+
+for (const item of new URLSearchParams(window.location.search)) {
+  if (item[0] === 'bot_id') bot_id = item[1];
+  if (item[0] === 'secretKey') secret = item[1];
+  if (item[0] === '#tgWebAppData') init = item[1];
+}
+
 export const config = {
   HOST: 'https://m.bot-t.com',
-  INIT_DATA: window.Telegram.WebApp.initData,
+  INIT_DATA: window.Telegram.WebApp.initData ?? init,
+  BOT_ID: bot_id,
+  SECRET: secret,
 };
+
 interface Config {
   HOST: string;
   INIT_DATA: string;
