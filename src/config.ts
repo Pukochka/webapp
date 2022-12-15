f0e7f5d84c6473581342d72e123df0a27694121e
Pukochka <tema.puko@gmail.com>
2022-12-15 20:35:24 +0300

    test url

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,6 @@ window.Telegram.WebApp.enableClosingConfirmation();
 let bot_id = '';
 let secret = '';
 let init = '';
-const test = {};
 
 for (const item of new URLSearchParams(window.location.search)) {
   if (item[0] === 'bot_id') bot_id = item[1];
@@ -28,7 +27,7 @@ export const config = {
   INIT_DATA:
     // test,
     // 'id=1028741753&first_name=Artemi&last_name=Puka&username=melart1&auth_date=1670798383&hash=733bd5e98b1aaafb95cba8c8697979ead69adb43a22ca25a8a55845568af8c34&id=1028741753&first_name=Artemi&last_name=Puka&username=melart1&auth_date=1670802314&hash=e52e106b00ace43fe37fe28096439e201035d67de86b835bcd7cfa877148814c&id=1028741753&first_name=Artemi&last_name=Puka&username=melart1&auth_date=1670804754&hash=9c88ef22397f0de5772e665b90d49ccad5f81e5826e6d5fb2af6006474c27f58',
-    window.Telegram.WebApp.initData.length
+    window.Telegram.WebApp.initData === ''
       ? window.Telegram.WebApp.initData
       : init,
   BOT_ID: bot_id,
